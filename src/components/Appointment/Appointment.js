27a4f4356747d4205ee9437b015cb9e334fb9509
taibislamdipu <mailtaibislam@gmail.com>
2020-10-02 22:15:34 +0600

    appoinment form create

--- src/components/Appointment/Appointment.js
+++ src/components/Appointment/Appointment.js
@@ -6,7 +6,26 @@ const Appointment = () => {
         <div>
             <Header></Header>
 
-            <h1>this is appointment page</h1>
+            <h1>This is appointment page</h1>
+
+            <div className="container w-25">
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" placeholder="YOUR NAME" />
+                    </div>
+                    <div class="form-group">
+                        <input type="number" class="form-control" id="number" placeholder="PHONE NUMBER" />
+                    </div>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="EMAIL" />
+                    </div>
+
+                    <div class="form-group">
+                        <input type="date" class="form-control" id="date" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/components/DentalService/DentalService.js
+++ src/components/DentalService/DentalService.js
@@ -5,10 +5,19 @@ import DentalServiceImg from '../../images/dentalService.png';
 
 import allServices from '../fakeData/allServices';
 
+
+
 const DentalService = () => {
 
     const service = allServices;
     const [services, setServices] = useState(service);
+
+    // function handleClick() {
+
+    //     console.log('The link was clicked.');
+    // }
+
+
 
     return (
         <main>
@@ -28,13 +37,14 @@ const DentalService = () => {
 
             <div>
                 {
-                    services.map(service => <div className="container mt-5">
-                        <div class="card w-25" >
-                            <div class="card-body">
-                                <h5 class="card-title">{service.name}</h5>
-                                <p class="card-text">{service.time}</p>
-                                <p class="card-text">{service.availability}</p>
+                    services.map(service => <div className="container mt-5" key={service.id}>
+                        <div className="card w-25" >
+                            <div className="card-body">
+                                <h5 className="card-title">{service.name}</h5>
+                                <p className="card-text">{service.time}</p>
+                                <p className="card-text">{service.availability}</p>
                                 <a href="/appointment" class="btn btn-primary">Go somewhere</a>
+                                {/* <button onClick={() => handleClick()} className="btn btn-primary">Book appointment</button> */}
                             </div>
                         </div>
                     </div>)
